a74eecd4f9fa561d5690bface987364870d281d9
Scott <hscottvo@gmail.com>
2024-02-21 13:01:06 -0800

    Add scrollEOF.nvim

--- options.lua
+++ options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
     clipboard = "unnamed",
+    scrolloff = 8,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
